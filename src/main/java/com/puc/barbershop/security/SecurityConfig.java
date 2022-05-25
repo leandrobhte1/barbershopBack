63b836bf592fb1afef8f4a693e6f522b71d9c61b
Leandro Oliveira Pereira <leandrobhte1@hotmail.com>
2022-05-25 09:45:40 -0300

    correcao salvar user

--- src/main/java/com/puc/barbershop/security/SecurityConfig.java
+++ src/main/java/com/puc/barbershop/security/SecurityConfig.java
@@ -40,9 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/api/login", "/api/refreshToken/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/login", "/api/user/save/**", "/api/refreshToken/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        //http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
